30eecf7bf82658a212a9d7940fe404634a3e658c
=sofysha91 <sofy.hernandeza@gmail.com>
2022-08-26 19:12:35 -0500

    Clean architecture structure

--- src/server.js
+++ src/server.js
@@ -4,9 +4,6 @@ const cors = require("cors");
 const app = express();
 
 //Routes
-const routerKoder = require("./routes/koder.route");
-const routerMentor = require("./routes/mentor.route");
-const routerGeneration = require("./routes/generation.route");
 const routerUser = require("./routes/user.route");
 const routerAuth = require("./routes/auth.route");
 
@@ -14,9 +11,6 @@ const routerAuth = require("./routes/auth.route");
 app.use(cors());
 app.use(express.json());
 
-app.use("/koders", routerKoder);
-app.use("/mentors", routerMentor);
-app.use("/generations", routerGeneration);
 app.use("/users", routerUser);
 app.use("/auth", routerAuth);
 
